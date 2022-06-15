3d83989ad17b51b83d16822d7a4326f3504c0a34
kVarunkk <varunkumawatleap2@gmail.com>
2022-06-15 12:29:24 +0530

    add and change files

---
+++ breakout.js
@@ -0,0 +1,223 @@
+const grid = document.querySelector('.grid')
+const scoreDisplay = document.querySelector('#score')
+const left = document.querySelector('.left')
+const right = document.querySelector('.right')
+const play = document.querySelector('.head')
+const restart = document.querySelector('.head2')
+const audio1 = document.querySelector('#audio1')
+const audio2 = document.querySelector('#audio2')
+const audio3 = document.querySelector('#audio3')
+const audio4 = document.querySelector('#audio4')
+const audio5 = document.querySelector('#audio5')
+const blockWidth = 100
+const blockHeight = 20
+const ballDiameter = 20
+const boardWidth = 560
+const boardHeight = 300
+let xDirection = -2
+let yDirection = 2
+
+const userStart = [230, 10]
+let currentPosition = userStart
+
+const ballStart = [270, 40]
+let ballCurrentPosition = ballStart
+
+let timerId
+let score = 0
+
+//my block
+class Block {
+  constructor(xAxis, yAxis) {
+    this.bottomLeft = [xAxis, yAxis]
+    this.bottomRight = [xAxis + blockWidth, yAxis]
+    this.topRight = [xAxis + blockWidth, yAxis + blockHeight]
+    this.topLeft = [xAxis, yAxis + blockHeight]
+  }
+}
+
+//all my blocks
+const blocks = [
+  new Block(10, 270),
+  new Block(120, 270),
+  new Block(230, 270),
+  new Block(340, 270),
+  new Block(450, 270),
+  new Block(10, 240),
+  new Block(120, 240),
+  new Block(230, 240),
+  new Block(340, 240),
+  new Block(450, 240),
+  new Block(10, 210),
+  new Block(120, 210),
+  new Block(230, 210),
+  new Block(340, 210),
+  new Block(450, 210),
+]
+
+//draw my blocks
+function addBlocks() {
+  for (let i = 0; i < blocks.length; i++) {
+    const block = document.createElement('div')
+    block.classList.add('block')
+    block.style.left = blocks[i].bottomLeft[0] + 'px'  
+    block.style.bottom = blocks[i].bottomLeft[1] + 'px'  
+    grid.appendChild(block)
+  }
+}
+addBlocks()
+
+//add user
+const user = document.createElement('div')
+user.classList.add('user')
+grid.appendChild(user)
+drawUser()
+
+//add ball
+const ball = document.createElement('div')
+ball.classList.add('ball')
+grid.appendChild(ball)
+drawBall()
+
+//move user
+function moveUser(e) {
+  switch (e.key) {
+    case 'ArrowLeft':
+      if (currentPosition[0] > 0) {
+        currentPosition[0] -= 10
+        drawUser()   
+      }
+      break
+    case 'ArrowRight':
+      if (currentPosition[0] < (boardWidth - blockWidth)) {
+        currentPosition[0] += 10
+        drawUser()   
+      }
+      break
+  }
+}
+document.addEventListener('keydown', moveUser)
+
+function moveUser1() {
+  
+  if (currentPosition[0] > 0) {
+    currentPosition[0] -= 10
+    drawUser()   
+  }
+}
+
+function moveUser2(){
+  if (currentPosition[0] < (boardWidth - blockWidth)) {
+    currentPosition[0] += 10
+    drawUser()   
+  }
+}  
+
+left.addEventListener('click', moveUser1)
+right.addEventListener('click', moveUser2)
+
+
+//draw User
+function drawUser() {
+  user.style.left = currentPosition[0] + 'px'
+  user.style.bottom = currentPosition[1] + 'px'
+}
+
+//draw Ball
+function drawBall() {
+  ball.style.left = ballCurrentPosition[0] + 'px'
+  ball.style.bottom = ballCurrentPosition[1] + 'px'
+}
+
+//move ball
+function moveBall() {
+    ballCurrentPosition[0] += xDirection
+    ballCurrentPosition[1] += yDirection
+    drawBall()
+    checkForCollisions()
+}
+
+
+play.addEventListener('click',function(){
+  timerId = setInterval(moveBall, 10)
+});
+
+const refreshPage = () => {
+  location.reload();
+}
+
+restart.addEventListener('click', refreshPage)
+
+//check for collisions
+function checkForCollisions() {
+  //check for block collision
+  for (let i = 0; i < blocks.length; i++){
+    if
+    (
+      (ballCurrentPosition[0] > blocks[i].bottomLeft[0] && ballCurrentPosition[0] < blocks[i].bottomRight[0]) &&
+      ((ballCurrentPosition[1] + ballDiameter) > blocks[i].bottomLeft[1] && ballCurrentPosition[1] < blocks[i].topLeft[1]) 
+    )
+      {
+      audio1.play();  
+      const allBlocks = Array.from(document.querySelectorAll('.block'))
+      allBlocks[i].classList.remove('block')
+      blocks.splice(i,1)
+      changeDirection()   
+      score++
+      scoreDisplay.innerHTML = score
+      if (blocks.length == 0) {
+        audio5.play();
+        scoreDisplay.innerHTML = 'You Win!'
+        clearInterval(timerId)
+        document.removeEventListener('keydown', moveUser)
+      }
+    }
+  }
+  // check for wall hits
+  if (ballCurrentPosition[0] >= (boardWidth - ballDiameter) || ballCurrentPosition[0] <= 0 || ballCurrentPosition[1] >= (boardHeight - ballDiameter))
+  {
+    audio2.play();
+    changeDirection()
+  }
+
+  //check for user collision
+  if
+  (
+    (ballCurrentPosition[0] > currentPosition[0] && ballCurrentPosition[0] < currentPosition[0] + blockWidth) &&
+    (ballCurrentPosition[1] > currentPosition[1] && ballCurrentPosition[1] < currentPosition[1] + blockHeight ) 
+  )
+  {
+    audio3.play();
+    changeDirection()
+  }
+
+  //game over
+  if (ballCurrentPosition[1] <= 0) {
+    clearInterval(timerId)
+    audio4.play();
+    scoreDisplay.innerHTML = 'You lose :('
+    document.removeEventListener('keydown', moveUser)
+    left.removeEventListener('click', moveUser1)
+    right.removeEventListener('click', moveUser2)
+  }
+}
+
+
+function changeDirection() {
+  if (xDirection === 2 && yDirection === 2) {
+    yDirection = -2
+    return
+  }
+  if (xDirection === 2 && yDirection === -2) {
+    xDirection = -2
+    return
+  }
+  if (xDirection === -2 && yDirection === -2) {
+    yDirection = 2
+    return
+  }
+  if (xDirection === -2 && yDirection === 2) {
+    xDirection = 2
+    return
+  }
+}
